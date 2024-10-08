7f89074df93d9af2e5ba734a998556b73c93efbb
KJM <chance0425@naver.com>
2024-10-08 15:00:27 +0900

    타임아웃 테스트2

--- Init.py
+++ Init.py
@@ -7,7 +7,7 @@ from PyQt5 import uic
 import Purchase
 
 #UI Loading
-Init_Class= uic.loadUiType("Init.ui")[0]
+Init_Class = uic.loadUiType("Init.ui")[0]
 
 #메인윈도우 설정
 class MainWindow(QMainWindow, Init_Class) :
@@ -15,21 +15,25 @@ class MainWindow(QMainWindow, Init_Class) :
         super().__init__()
         self.setupUi(self)
 
-        self.mainPage_toInit()
+        self.init_setting()
 
 #def
     def set_MainPage_Index(self, index) :
         self.mainPage.setCurrentIndex(index)
 
-#180초 타이머 설정
+    #기초 세팅값 설정
+    def init_setting(self) :
+        self.lcd_Timer.display(180)
+        self.set_MainPage_Index(0)
+
+    #180초 타이머 설정
     def timeout_Start(self) :
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_timer)
         self.timer.start(1000)
-        self.remaining_time = 5
+        self.remaining_time = 3 * 60
 
     def timeout_Return(self) :
-        QMessageBox.about(self, 'test2', 'test2')
         self.set_MainPage_Index(0)
 
     def update_timer(self) :
@@ -37,13 +41,20 @@ class MainWindow(QMainWindow, Init_Class) :
             self.remaining_time -= 1
             self.lcd_Timer.display(self.remaining_time)
         else :
-            self.timer.stop()
+            self.stop_timer()
             self.timeout_Return()
+
+    def stop_timer(self) :
+        self.timer.stop()
         
 #Buttons
     #(시작화면)으로 이동
     def mainPage_toInit(self) :
-        self.set_MainPage_Index(0)
+        try :
+            self.stop_timer()
+            self.set_MainPage_Index(0)
+        except : 
+            self.set_MainPage_Index(0)
 
     #(일반주문, 음성주문 선택화면)으로 이동
     def mainPage_toSelect(self) :
@@ -51,6 +62,7 @@ class MainWindow(QMainWindow, Init_Class) :
 
     #(일반주문화면)으로 이동
     def mainPage_toDefault(self) :
+        self.lcd_Timer.display(180)
         self.timeout_Start()
         self.set_MainPage_Index(2)
 
