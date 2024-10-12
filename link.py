6bca39d90aa2273cc751c6ae4c304033be7311dd
LeeHuSeong <84837365+LeeHuSeong@users.noreply.github.com>
2024-10-12 20:22:34 +0900

    edit link.py
    겹치는 코드 삭제

--- link.py
+++ link.py
@@ -24,16 +24,6 @@ def close_connection(conn):
         conn.close()
         print("MySQL 데이터베이스와의 연결이 종료됨.")
 
-conn = create_connection()
-close_connection(conn)
 
-conn = mysql.connector.connect(
-    host = "127.0.0.1:3303",
-    user = "root",        # 본인의 DB 사용자 이름으로 수정
-    password = "0000", # 본인의 DB 비밀번호로 수정
-    database = "test_one"  # 본인의 DB 이름으로 수정
-)
-
-cursor= conn.cursor()
 conn.commit()
 conn.close()
