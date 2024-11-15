54ae74f30d210ed26fd19b170e501d43bd4fd3e7
KJM <chance0425@naver.com>
2024-11-15 13:36:03 +0900

    optimization_TEST_3/실행시간 측정

--- front/menu/menu_Init.py
+++ front/menu/menu_Init.py
@@ -4,10 +4,14 @@ from PyQt5.QtCore import *
 import front
 import back1
 
+import time #for Test
+
 #형식: 메뉴이름, 가격, 이미지경로, 분류
 ##menudata[3]
 
 def menuWidget_Load(self, type) :
+        start = time.time()#
+
         self.menuList.clear()
         menuDB = []
         if 'menuDB_origin' not in locals()  :
@@ -29,8 +33,13 @@ def menuWidget_Load(self, type) :
                 else :
                     pass
 
+        ## menuDB = [menuType: menuList[]]
+        start2 = time.time()
         for list in menuDB :
             list.append(optionDB[list[0]][1])
+        end2 = time.time()
+        print(f'실행 시간: {end2 - start2}')#
+
     
         menuData = []
 
@@ -45,3 +54,5 @@ def menuWidget_Load(self, type) :
             self.menuList.addItem(item)
             self.menuList.setItemWidget(item, item_Widget)
 
+        end = time.time()#
+        print(f'메뉴 로딩 시간: {end - start}')#
