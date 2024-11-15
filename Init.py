e42afbe69493ba751b05771a53e90c5284fcfd61
KJM <chance0425@naver.com>
2024-11-16 03:04:37 +0900

    update_comment

--- Init.py
+++ Init.py
@@ -11,13 +11,8 @@ import back1
 #UI Loading
 Init_Class = uic.loadUiType("front/UI/Init.ui")[0]
 
-#메인윈도우 설정    
 class MainWindow(QMainWindow, Init_Class) :
-#variables
     totalPrice = 0
-    
-    def Reset_lcd_Price(self) :
-        self.lcd_Price.display(self.totalPrice)
 
     def __init__(self):
         super().__init__()
@@ -25,7 +20,7 @@ class MainWindow(QMainWindow, Init_Class) :
 
         self.init_setting()
 
-    #Initial_settings (execute once)
+#Initial_Settings(execute once)/초기 설정(한번만 실행됨)
     def init_setting(self) :
         #Timer_Init
         self.timer = front.timeoutClass(self)
@@ -39,16 +34,22 @@ class MainWindow(QMainWindow, Init_Class) :
         front.menuWidget_Load(self, 'ALL')
 
         self.set_MainPage_Index(0)
+#Initial_Settings
 
-    #need to change def name
+#Def_ETC
+    #Timer_AddTime/타이머 시간추가
     def add_timer(self) :
         self.timer.remain_Time += self.timer.add_Time
         self.lcd_Timer.display(self.timer.remain_Time)
+    #Reset_PriceLCD/가격표시LCD 새로고침
+    def Reset_lcd_Price(self) :
+        self.lcd_Price.display(self.totalPrice)
+#Def_ETC
 
+#Move_Page/화면 전환(stackedWidget 관련)
     def set_MainPage_Index(self, index) :
         self.mainPage.setCurrentIndex(index)
 
-#Move_Page/화면 전환(stackedWidget 관련)
     #move to initPage
     def mainPage_toInit(self) :
         try :
@@ -114,7 +115,6 @@ class MainWindow(QMainWindow, Init_Class) :
             purchaseWindow = front.purchaseWindow(totalOrderData, self)
             purchaseWindow.order_Price.display(self.totalPrice)
             purchaseWindow.showModal()
-
 #btn_ETC
 
 ######################################################
