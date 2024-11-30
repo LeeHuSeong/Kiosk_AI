ab5918e77010748f24c1d80062bcc280cf5d8979
KJM <chance0425@naver.com>
2024-12-01 00:03:53 +0900

    gpt_api 수정_0

--- front/patois_Alter.py
+++ front/patois_Alter.py
@@ -11,13 +11,15 @@ class use_OpenAI:
             messages=[
                 {"role": "system", "content": "사투리나 부정확한 텍스트를 입력해줄테니 표준어 텍스트로 바꿔줘"},
                 {"role": "system", "content": "마침표는 빼고 (온도) 메뉴 수량 형식으로 말해줘"},
-                {"role": "system", "content": "메뉴는 카페 음료, 디저트야"},
                 {"role": "user", "content": f"{text}"}
                 ]
             )
             result = response.choices[0].message.content
             return result
         
-        except :
-            print("gpt_api 키 값이 올바르지 않습니다.")
+        except Exception as e:
+            print(f"gpt_api 키 값이 올바르지 않습니다.\n{e}")
             return text
+
+a = use_OpenAI.gpt_Patois_Correction('떠거운 카피 한잔')
+print(a)
